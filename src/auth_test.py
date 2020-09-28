ab0b4ad5ddde98ab3fa05a05a4f8a1743fc2d3d7
Andrew Xie <x.and.i.rew.e@gmail.com>
2020-09-28 21:13:55 +1000

    added a test

--- src/auth_test.py
+++ src/auth_test.py
@@ -1,6 +1,10 @@
 # Test file for auth.py
+from auth import auth_login, auth_logout, auth_register
 
-import auth
-
-def test():
-    pass
+def test_valid_auth():
+    # User 1
+    user1_email = "test@gmail.com"
+    user1_password = "password"
+    auth_register(user1_email, user1_password, "Ben", "Bill")
+    user1 = auth_login(user1_email, user1_password)
+    assert auth_logout(user1['token']) == {'is_success': True}
