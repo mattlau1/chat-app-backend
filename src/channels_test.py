717ea0088250007aa4186db190a716eb1c56b52e
z5319929 <swxergamer64@gmail.com>
2020-09-29 19:54:20 +1000

    create valid channel test

--- src/channels_test.py
+++ src/channels_test.py
@@ -1,6 +1,17 @@
 # Test file for channels.py
 
-import channels
+import pytest
+from channels import *
+from error import InputError
 
-def test():
-    pass
+def test_create_valid_names():
+    # create channel and set public to true
+    channels_create("token", "FirstChannel", True)
+    channels_create("token", "UNSW discussion",  True)
+    channels_create("token", "Private", False)
+    clear()
+
+def test_create_invalid_names():
+    # # create a channel that has more than 20 chracters in name
+    # with pytest.raises(InputError):
+    #     channels_create("token", "", True)
