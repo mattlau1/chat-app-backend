10c030c77cdcc77e2574a824e72e02910a19ccbb
Matthew Lau <z5309299@unsw.edu.au>
2020-09-29 19:52:45 +1000

    added imports for test file

--- src/channels_test.py
+++ src/channels_test.py
@@ -1,6 +1,9 @@
 # Test file for channels.py
 
-import channels
+import pytest
+from error import InputError, AccessError
+from channels import channels_list, channels_listall, channels_create
+from other import clear
 
 def test():
     pass
