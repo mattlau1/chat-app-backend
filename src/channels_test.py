718b480ac572c7fdfeaf9e74e8d190ebe165e3d2
Steven Long Nguyen <swxergamer64@gmail.com>
2020-10-02 18:52:33 +1000

    fixed tokens for test cases

--- src/channels_test.py
+++ src/channels_test.py
@@ -5,12 +5,13 @@ from data import data
 from error import InputError, AccessError
 from channels import channels_list, channels_listall, channels_create
 from other import clear
+from auth import auth_register
 
 def test_create_valid_names():
     # create channel and set public to true
-    channels_create("token", "FirstChannel", True)
-    channels_create("token", "UNSW discussion",  True)
-    channels_create("token", "Private", False)
+    channels_create(user['token'], "FirstChannel", True)
+    channels_create(user['token'], "UNSW discussion",  True)
+    channels_create(user['token'], "Private", False)
     assert data['channels'] == [{'id': 1, 'name': 'FirstChannel'}, {'id': 2, 'name': 'UNSW discussion'}, {'id': 3, 'name': 'Private'}]
     clear()
 
@@ -18,27 +19,27 @@ def test_create_long_names():
     # create a channel that has more than 20 characters (max length) in name
     long_name = "abcdefghijklmnopqrstuvwxyz"
     with pytest.raises(InputError):
-        channels_create("token", long_name, True)
+        channels_create(user['token'], long_name, True)
     long_name = "thisnameisverylong000"
     with pytest.raises(InputError):
-        channels_create("token", long_name, True)
+        channels_create(user['token'], long_name, True)
     long_name = "ijxiqwji9xq9iqdjim9dwq89n189819me781neuensa9xaj9xd"
     with pytest.raises(InputError):
-        channels_create("token", long_name, False)
+        channels_create(user['token'], long_name, False)
     clear()
 
 def test_create_empty_name():
     with pytest.raises(InputError):
-        channels_create("token", '', True)
+        channels_create(user['token'], '', True)
     with pytest.raises(InputError):
-        channels_create("token", '', False)
+        channels_create(user['token'], '', False)
     clear()
 
 def test_create_whitespace_name():
     with pytest.raises(InputError):
-        channels_create("token", ' ', True)
+        channels_create(user['token'], ' ', True)
     with pytest.raises(InputError):
-        channels_create("token", '          ', True)
+        channels_create(user['token'], '          ', True)
     with pytest.raises(InputError):
-        channels_create("token", '      ', False)
+        channels_create(user['token'], '      ', False)
     clear()
