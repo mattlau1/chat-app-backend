d4e23baeea5b68312b3fa3bac4fc664fc78c8e49
Steven Long Nguyen <swxergamer64@gmail.com>
2020-10-02 18:59:24 +1000

    forgot to include auth_register

--- src/channels_test.py
+++ src/channels_test.py
@@ -9,6 +9,7 @@ from auth import auth_register
 
 def test_create_valid_names():
     # create channel and set public to true
+    user = auth_register('bobsmith@gmail.com', 'password', 'Bob', 'Smith')
     channels_create(user['token'], "FirstChannel", True)
     channels_create(user['token'], "UNSW discussion",  True)
     channels_create(user['token'], "Private", False)
@@ -17,6 +18,7 @@ def test_create_valid_names():
 
 def test_create_long_names():
     # create a channel that has more than 20 characters (max length) in name
+    user = auth_register('jesschen@gmail.com', 'password', 'Jess', 'Chen')
     long_name = "abcdefghijklmnopqrstuvwxyz"
     with pytest.raises(InputError):
         channels_create(user['token'], long_name, True)
@@ -29,6 +31,7 @@ def test_create_long_names():
     clear()
 
 def test_create_empty_name():
+    user = auth_register('user1@gmail.com', 'password1', 'John', 'Smith')
     with pytest.raises(InputError):
         channels_create(user['token'], '', True)
     with pytest.raises(InputError):
@@ -36,6 +39,7 @@ def test_create_empty_name():
     clear()
 
 def test_create_whitespace_name():
+    user = auth_register('petermichaels@gmail.com', 'password', 'Peter', 'Michaels')
     with pytest.raises(InputError):
         channels_create(user['token'], ' ', True)
     with pytest.raises(InputError):
