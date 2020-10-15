60caecb52b55fcaa06cf935e3d256dde0c8b99b9
Andrew Xie <x.and.i.rew.e@gmail.com>
2020-10-15 22:39:21 +1100

    Added SHA256 password encryption, JWT-encoded tokens and fixed general formatting (pylint and docstrings)

--- src/auth.py
+++ src/auth.py
@@ -1,26 +1,43 @@
+import hashlib
+import jwt
 from data import (
-    data, valid_email, user_with_email, user_with_token,
+    PRIVATE_KEY, data, valid_email, user_with_email, user_with_token,
     user_email_list, user_handle_list, user_update_token,
 )
 from error import InputError, AccessError
 
 # Generates a token for a registered user
-def generate_token(id):
-    # import user_with_id and maybe datetime for future iterations
-    return str(id)
+def generate_token(u_id):
+    '''
+    Generates a JSON Web Token (JWT) encoded token for a given user id
+    Params: u_id (int)
+    Returns: JWT-encoded token (str)
+    '''
+    return jwt.encode({'u_id': u_id}, PRIVATE_KEY, algorithm='HS256').decode('utf-8')
 
 # Generates a handle for a user
-def generate_handle(id, name_first, name_last):
+def generate_handle(u_id, name_first, name_last):
+    '''
+    Generates a unique handle for a given user
+    Params: id (int), name_first (str), name_last (str)
+    Returns: handle_string (str)
+    '''
     # First 20 characters of concatenation of name_first and name_last
     handle_string = (name_first.lower() + name_last.lower())[:20]
     # Ensure unique
     while handle_string in user_handle_list():
-        handle_string = (str(id) + handle_string)[:20]
+        handle_string = (str(u_id) + handle_string)[:20]
     return handle_string
 
 
 def auth_login(email, password):
+    '''
+    Logs a user in
+    Params: email (str), password (str)
+    Returns: u_id (int) and token (str) as a dict
+    '''
     user = user_with_email(email)
+    encrypted_password = hashlib.sha256(password.encode()).hexdigest()
     # Error check
     if not valid_email(email):
         # Invalid email format
@@ -28,14 +45,13 @@ def auth_login(email, password):
     elif user is None:
         # Unregistered email
         raise InputError
-    elif user['password'] != password:
+    elif user['password'] != encrypted_password:
         # Incorrect password
         raise InputError
-    
+
     # Update token
     token = generate_token(user['id'])
-    updated = user_update_token(user['id'], token)
-    assert updated['update_success'] == True
+    user_update_token(user['id'], token)
     assert user_with_email(email)['token'] == token
 
     return {
@@ -45,22 +61,31 @@ def auth_login(email, password):
 
 
 def auth_logout(token):
+    '''
+    Logs a user out
+    Params: token (str)
+    Returns: is_success (bool) as a dict
+    '''
     user = user_with_token(token)
     # Check for valid token
     if user is None:
         raise AccessError
-    
+
     # Update user token
-    updated = user_update_token(user['id'], '')
-    assert updated['update_success'] == True
+    user_update_token(user['id'], '')
     assert user_with_email(user['email'])['token'] == ''
-    
+
     return {
         'is_success': True,
     }
 
 
 def auth_register(email, password, name_first, name_last):
+    '''
+    Registers a user
+    Params: email (str), password (str), name_first (str), name_last (str)
+    Returns: u_id generated (int) and token generated (str) as a dict
+    '''
     # Error check
     if not valid_email(email):
         # Invalid email format
@@ -79,20 +104,22 @@ def auth_register(email, password, name_first, name_last):
         raise InputError
 
     # Register new user
-    id = len(data['users']) + 1
-    token = generate_token(id)
+    u_id = len(data['users']) + 1
+    token = generate_token(u_id)
+    encrypted_password = hashlib.sha256(password.encode()).hexdigest()
+
     # Append user information to data
     data['users'].append({
-        'id': id,
+        'id': u_id,
         'email': email,
-        'password': password,
+        'password': encrypted_password,
         'name_first': name_first,
         'name_last': name_last,
-        'handle': generate_handle(id, name_first, name_last),
+        'handle': generate_handle(u_id, name_first, name_last),
         'token': token,
     })
-    
+
     return {
-        'u_id': id,
+        'u_id': u_id,
         'token': token,
     }
