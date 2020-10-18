5cb3abfa3eb18037e11094f458dada776ae2240f
Steven Long Nguyen <swxergamer64@gmail.com>
2020-10-19 02:21:19 +1100

    created user_test.py

---
+++ src/user_test.py
@@ -0,0 +1,11 @@
+import pytest
+from other import clear
+from auth import auth_register
+from error import InputError, AccessError
+
+def test_valid_user():
+    pass
+
+def test_invalid_user();
+    pass
+
