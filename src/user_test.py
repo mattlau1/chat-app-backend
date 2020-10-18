cdbd6e9489316dadbce1820718e8d6e6ba670f1f
Steven Long Nguyen <swxergamer64@gmail.com>
2020-10-19 02:38:16 +1100

    added docstring for valid and invalid users

--- src/user_test.py
+++ src/user_test.py
@@ -1,11 +1,23 @@
 import pytest
+from user import user_profile
 from other import clear
 from auth import auth_register
 from error import InputError, AccessError
 
 def test_valid_user():
+    '''
+    Create a valid user
+    Return the user detail associated with
+    given token and u_id
+    '''
+    user = auth_register('stvnnguyen69@hotmail.com', 'password', 'Steven', 'Nguyen')
     pass
 
 def test_invalid_user();
+    '''
+    Returns invalid user
+    when providing token and u_id that has not been
+    created yet
+    '''
     pass
 
