ee718c9c3615b24fa092195c788f6ba08073f9e7
Steven Long Nguyen <swxergamer64@gmail.com>
2020-10-19 21:26:09 +1100

    fixed error concerning variable name

--- src/user_test.py
+++ src/user_test.py
@@ -19,26 +19,29 @@ def test_valid_user():
     clear()
     # standard user
     user = auth_register('stvnnguyen69@hotmail.com', 'password', 'Steven', 'Nguyen')
-    user_profile = user_profile(user['token'], user['u_id'])
-    assert user_profile['user']['name_first'] == "Steven"
-    assert user_profile['user']['name_last'] == "Nguyen"
-    assert user_profile['user']['u_id'] == 1
-    assert user_profile['user']['email'] == 'stvnnguyen69@hotmail.com'
-    assert user_profile['user']['handle'] == '1stevennguyen'
+    profile = user_profile(user['token'], user['u_id'])
+    assert profile['user']['name_first'] == "Steven"
+    assert profile['user']['name_last'] == "Nguyen"
+    assert profile['user']['u_id'] == 1
+    assert profile['user']['email'] == 'stvnnguyen69@hotmail.com'
+    assert profile['user']['handle'] == '1stevennguyen'
 
     # user with long name to check handle name
-    user2 = auth_register('madeulook100@gmail.com', 'madeulook', 'Verylongfirstname', 'Verylonglastname')
-    user_profile = user_profile(user2['token'], user2['u_id'])
-    assert user_profile['user']['name_first'] == "Verylongfirstname"
-    assert user_profile['user']['name_last'] == "Verylonglastname"
-    assert user_profile['user']['u_id'] == 2
-    assert user_profile['user']['email'] == 'madeulook100@gmail.com'
-    assert user_profile['user']['handle'] == '2verylongfirstnameve'
+    user2 = auth_register(
+        'madeulook100@gmail.com', 'madeulook',
+        'Verylongfirstname', 'Verylonglastname'
+    )
+    profile = user_profile(user2['token'], user2['u_id'])
+    assert profile['user']['name_first'] == "Verylongfirstname"
+    assert profile['user']['name_last'] == "Verylonglastname"
+    assert profile['user']['u_id'] == 2
+    assert profile['user']['email'] == 'madeulook100@gmail.com'
+    assert profile['user']['handle'] == '2verylongfirstnameve'
 
 def test_invalid_user():
     '''
     Raise exception when providing token and u_id that has not been created yet.
-
+    Create valid users but call user detail with incorrrect u_id and token
     '''
     clear()
     # retrieving information without registering
@@ -73,76 +76,77 @@ def test_invalid_user():
 
 def test_valid_setnames():
     '''
-    Change name of the user associated with the token
+    Change name of the user with valid name and check that the id stays the same
+    Use edge case to change into new name such as 1 character or exactly 50 characters
     '''
     clear()
     # original user
     user = auth_register('blastfire97@gmail.com', 'p@ssw0rd', 'Apple', 'Appleson')
-    user_profile = user_profile(user['token'], user['u_id'])
-    assert user_profile['user']['name_first'] == "Apple"
-    assert user_profile['user']['name_last'] == "Appleson"
-    assert user_profile['user']['u_id'] == 1
+    profile = user_profile(user['token'], user['u_id'])
+    assert profile['user']['name_first'] == "Apple"
+    assert profile['user']['name_last'] == "Appleson"
+    assert profile['user']['u_id'] == 1
 
     # same token, id but different username
     user_profile_setname(user['token'], 'Banana', 'Bananason')
-    assert user_profile['user']['name_first'] == "Banana"
-    assert user_profile['user']['name_last'] == "Bananason"
-    assert user_profile['user']['u_id'] == 1
+    assert profile['user']['name_first'] == "Banana"
+    assert profile['user']['name_last'] == "Bananason"
+    assert profile['user']['u_id'] == 1
 
     # change name multiple times under the same token
     user2 = auth_register('samsunggalaxy01@gmail.com', 'password', 'Orange', 'Orangeson')
-    user_profile = user_profile(user2['token'], user2['u_id'])
-    assert user_profile['user']['name_first'] == "Orange"
-    assert user_profile['user']['name_last'] == "Orangeson"
-    assert user_profile['user']['u_id'] == 2
+    profile = user_profile(user2['token'], user2['u_id'])
+    assert profile['user']['name_first'] == "Orange"
+    assert profile['user']['name_last'] == "Orangeson"
+    assert profile['user']['u_id'] == 2
     user_profile_setname(user2['token'], 'Strawberry', 'Strawberryson')
-    assert user_profile['user']['name_first'] == "Strawberry"
-    assert user_profile['user']['name_last'] == "Strawberryson"
-    assert user_profile['user']['u_id'] == 2
+    assert profile['user']['name_first'] == "Strawberry"
+    assert profile['user']['name_last'] == "Strawberryson"
+    assert profile['user']['u_id'] == 2
     user_profile_setname(user2['token'], 'Michael', 'Michaelson')
-    assert user_profile['user']['name_first'] == "Michael"
-    assert user_profile['user']['name_last'] == "Michaelson"
-    assert user_profile['user']['u_id'] == 2
+    assert profile['user']['name_first'] == "Michael"
+    assert profile['user']['name_last'] == "Michaelson"
+    assert profile['user']['u_id'] == 2
 
     # changing name with 1 character 
     user3 = auth_register('mmmonkey97@hotmail.com', 'password', 'John', 'Johnson')
-    user_profile = user_profile(user3['token'], user3['u_id'])
-    assert user_profile['user']['name_first'] == "John"
-    assert user_profile['user']['name_last'] == "Johnson"
+    profile = user_profile(user3['token'], user3['u_id'])
+    assert profile['user']['name_first'] == "John"
+    assert profile['user']['name_last'] == "Johnson"
     user_profile_setname(user3['token'], "A", "B")
-    assert user_profile['user']['name_first'] == "A"
-    assert user_profile['user']['name_last'] == "B"
+    assert profile['user']['name_first'] == "A"
+    assert profile['user']['name_last'] == "B"
 
     # changing name with exactly 50 characters
     long_first = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     long_last = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
     user4 = auth_register('austinnistau@hotmai.com', 'password', 'Austin', 'Austinson')
-    user_profile = user_profile(user4['token'], user4['u_id'])
-    assert user_profile['user']['name_first'] == "Austin"
-    assert user_profile['user']['name_last'] == "Austinson"
+    profile = user_profile(user4['token'], user4['u_id'])
+    assert profile['user']['name_first'] == "Austin"
+    assert profile['user']['name_last'] == "Austinson"
     user_profile_setname(user4['token'], long_first, long_last)
-    assert user_profile['user']['name_first'] == long_first
-    assert user_profile['user']['name_last'] == long_last
+    assert profile['user']['name_first'] == long_first
+    assert profile['user']['name_last'] == long_last
 
-def test_invalid_setnames();
+def test_invalid_setnames():
     '''
     Raise exception when changing the name of user using invalid format
-    such as a very long name, very short or empty name
+    such as names with more than 50 characers long, empty name and empty spaces
     '''
     clear()
     # changing name to more than 50 characters long
     long_first = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    long_second = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+    long_last = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
     user = auth_register('hardcoregamer02@hotmail.com', 'password', 'Raymond', 'Raymondson')
-    user_profile = user_profile(user['token'], user['u_id'])
-    assert user_profile['user']['name_first'] == "Raymond"
-    assert user_profile['user']['name_last'] == "Raymondson"
+    profile = user_profile(user['token'], user['u_id'])
+    assert profile['user']['name_first'] == "Raymond"
+    assert profile['user']['name_last'] == "Raymondson"
     with pytest.raises(InputError):
-        user_profile_setname(user['token'], first_name, long_second)
+        user_profile_setname(user['token'], long_first, long_last)
 
     # changing name with empty space
     user2 = auth_register('mmmonkey97@hotmail.com', 'password', 'John', 'Johnson')
-    user_profile = user_profile(user2['token'], user2['u_id'])
+    profile = user_profile(user2['token'], user2['u_id'])
     with pytest.raises(InputError):
         user_profile_setname(user2['token'], '', '')
     with pytest.raises(InputError):
