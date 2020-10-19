58a7bd6249990370b43cefbbffc5ff82892b4da8
z5308941 <edward.lu1000@gmail.com>
2020-10-20 03:00:20 +1100

    Added tests for admin_userpermission_change

--- src/other_test.py
+++ src/other_test.py
@@ -1,40 +1,112 @@
 ''' Test file for other.py '''
 import pytest
 from auth import auth_register
-from channel import channel_invite, channel_details
-from channels import channels_create, channels_list
+from channel import channel_invite, channel_join, channel_leave
+from channels import channels_create
 from message import message_send, message_remove, message_edit
-from other import clear, search
+from other import clear, search, admin_userpermission_change
 from error import InputError, AccessError
 
 
-def test_clear():
+def test_admin_userpermission_change_to_owner():
     '''
-    Test that clear removes the users[], channels[] and resets the latest_message_id
+    Test that admin_userpermission_change can set permissions of the user with the given user ID
+    from member to owner permissions.
     '''
-    f_owner = auth_register('admin@gmail.com', 'password', 'Bob', 'Bob')
-    f_channel = channels_create(f_owner['token'], 'Channel 1', True)
-
-    details = channel_details(f_owner['token'], f_channel['channel_id'])
-    assert len(details['all_members']) == 1
-    assert len(details['owner_members']) == 1
-
-    # random_user = auth_register('random@gmail.com', 'password', 'Random', 'User')
-    # channel_invite(f_owner['token'], f_channel['channel_id'], random_user['u_id'])
-    # assert len(details['all_members']) == 2
-    # assert len(details['owner_members']) == 1
-    
-    assert len(channels_list(f_owner['token'])['channels']) == 1
-    
-    m_id = message_send(f_owner['token'], f_channel['channel_id'], 'First message')
-    
-    assert m_id['message_id'] == 1
-    
     clear()
-    # assert len(details['all_members']) == 0
-    # assert len(details['owner_members']) == 0
-    # assert len(channels_list(f_owner['token'])['channels']) == 0
-    # assert m_id['message_id'] == 0
+    user1 = auth_register('billgates@gmail.com', 'password', 'Bill', 'Gates')
+    user2 = auth_register('steveballmer@gmail.com', 'password', 'Steve', 'Ballmer')
+
+    # First user (a Flockr owner) creates a private channel
+    f_channel = channels_create(user1['token'], 'Private Channel', False)
+    # Second user is unable to join the private channel as they are not a Flockr owner
+    with pytest.raises(AccessError):
+        channel_join(user2['token'], f_channel['channel_id'])
+
+    # First user changes permissions of second user to make them a Flockr owner
+    admin_userpermission_change(user1['token'], user2['u_id'], 1)
+
+    # Check that second user is now a Flockr owner 
+    # (verified by now being able to join the private channel)
+    channel_join(user2['token'], f_channel['channel_id'])
+
+
+def test_admin_userpermission_change_to_member():
+    '''
+    Test that admin_userpermission_change can set permissions of the user with the given user ID
+    from owner to member permissions.
+    '''
+    clear()
+    user1 = auth_register('billgates@gmail.com', 'password', 'Bill', 'Gates')
+    user2 = auth_register('steveballmer@gmail.com', 'password', 'Steve', 'Ballmer')
+
+    # First user changes permissions of second user to make them a Flockr owner
+    admin_userpermission_change(user1['token'], user2['u_id'], 1)
+
+    # Second user creates a private channel
+    f_channel = channels_create(user2['token'], 'Private Channel', False)
+    # First user is able to join the private channel as they are a Flockr owner
+    channel_join(user1['token'], f_channel['channel_id'])
+    channel_leave(user1['token'], f_channel['channel_id'])
+
+    # Second user changes permissions of first user to make them a member
+    admin_userpermission_change(user2['token'], user1['u_id'], 2)
+
+    # Check that first user is now a member
+    # (verified by now not being able to join the private channel)
+    with pytest.raises(AccessError):
+        channel_join(user1['token'], f_channel['channel_id'])
+
+
+def test_admin_userpermission_change_invalid_user_id():
+    '''
+    Test that an InputError is raised when admin_userpermission_change 
+    is given a u_id that does not refer to a valid user
+    '''
+    clear()
+    f_owner = auth_register('admin@gmail.com', 'password', 'Bob', 'Bob')
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], 2, 1)
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], 0, 1)
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], 100, 1)
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], -5, 1)
+
+
+def test_admin_userpermission_change_invalid_permission_id():
+    '''
+    Test that an InputError is raised when admin_userpermission_change 
+    is given a permission_id that does not refer to a valid permission
+    '''
+    clear()
+    f_owner = auth_register('admin@gmail.com', 'password', 'Bob', 'Bob')
+    random_user = auth_register('timhall@gmail.com', 'password', 'Tim', 'Hall')
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], random_user['u_id'], 0)
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], random_user['u_id'], 3)
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], random_user['u_id'], 100)
+    with pytest.raises(InputError):
+        admin_userpermission_change(f_owner['token'], random_user['u_id'], -5)
+
+
+def test_admin_userpermission_change_user_not_owner():
+    '''
+    Test that an AccessError is raised when the authorised user calling
+    admin_userpermission_change does not have owner permissions
+    '''
+    clear()
+    f_owner = auth_register('admin@gmail.com', 'password', 'Bob', 'Bob')
+    member1 = auth_register('timhall@gmail.com', 'password', 'Tim', 'Hall')
+    member2 = auth_register('kimsean@gmail.com', 'password', 'Kim', 'Sean')
+    
+    with pytest.raises(AccessError):
+        admin_userpermission_change(member1['token'], f_owner['u_id'], 2)
+    with pytest.raises(AccessError):
+        admin_userpermission_change(member2['token'], member1['u_id'], 1)
 
 
 def test_search_invalid():
