d75a5fa13898cdfe4270a6e8b96672b38def9acf
Steven Long Nguyen <swxergamer64@gmail.com>
2020-10-24 00:56:17 +1100

    created http test for user profiles

---
+++ src/user_http_test.py
@@ -0,0 +1,34 @@
+import pytest
+import re
+from subprocess import Popen, PIPE
+import signal
+from time import sleep
+import requests
+import json
+
+# Use this fixture to get the URL of the server.
+@pytest.fixture
+def url():
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "user.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
+
+# IMPLEMENT TEST FUNCTIONS HERE
+
+def user_profile_test():
+
+    
